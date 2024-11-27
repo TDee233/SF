8ef4a98e124d80c4ce8a31cbba00bbbe631cbc8e
Trofimov Dmitry <winprint63@gmail.com>
2024-11-27 21:22:23 +0400

    file class upgrade, dir methods

--- src/mod9/FileWorks.java
+++ src/mod9/FileWorks.java
@@ -5,15 +5,24 @@ import java.io.IOException;
 
 public class FileWorks {
     public static void main(String[] args) throws IOException {
-        File fileTester = new File("C:\\Test","Test.txt");
-
+        File fileTester = new File("C:\\Test");
+        File dirTester = new File("C:\\Test\\Test1");
 
         System.out.println(fileTester.canExecute());
         System.out.println(fileTester.getPath());
         System.out.println(fileTester.exists());
 
-        boolean isCreated = fileTester.createNewFile();
-        System.out.println(isCreated);
+//        boolean isCreated = fileTester.createNewFile();
+//        boolean isMake = dirTester.mkdir();
+//        System.out.println(isCreated);
+//        System.out.println(isMake);
+
+        String[] files = fileTester.list();
+        for (String fileName : files) {
+            System.out.println(fileName);
+        }
+//        boolean isDel = dirTester.delete();
+//        System.out.println(isDel);
 
     }
 }
